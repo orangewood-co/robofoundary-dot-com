2ec8ddccbbb4f8e3ebfb31546436f1be35d4142e
owl <rohit.kartik9@gmail.com>
2025-09-09 18:13:49 +0530

    fix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,37 @@ const spaceGrotesk = Space_Grotesk({
 });
 
 export const metadata = {
+  metadataBase: new URL("https://vega-education.com"),
   title: "Vega",
   description:
-    "At Vega, we bridge the gap between theory and real-world tech. From intelligent robotic systems to hands-on learning, we empower the next-gen of innovators with tools that matter.  ",
+    "At Vega, we bridge the gap between theory and real-world tech. From intelligent robotic systems to hands-on learning, we empower the next-gen of innovators with tools that matter.",
+  openGraph: {
+    title: "Vega",
+    description:
+      "At Vega, we bridge the gap between theory and real-world tech. Hands-on learning, workshops, and community-driven programs.",
+    url: "https://vega-education.com",
+    siteName: "Vega",
+    images: [
+      {
+        url: "/vega-main.png",
+        width: 1200,
+        height: 630,
+        alt: "Vega - Hands-on Tech Learning",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Vega",
+    description:
+      "Hands-on learning, workshops, and community-driven programs from Vega.",
+    image: "/vega-main.png", // must be in /public
+  },
+  icons: {
+    icon: "/vega.png",
+    apple: "/vega.png",
+  },
 };
 
 export default function RootLayout({
